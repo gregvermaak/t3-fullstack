77883f8dbc6ff3370324837dcba2342ee701eb18
Greg Vermaak <gregvermaak@Gregs-MacBook-Pro.local>
2023-05-01 11:40:10 +1200

    Initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,35 @@
+import { type Config } from "tailwindcss";
+
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "5vw",
+        screens: {
+          "2xl": "1400px",
+        },
+      },
+      screens: {
+        xxs: "320px",
+        xs: "420px",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
